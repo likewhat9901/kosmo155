9f9d30ab09f62c072ae0968938a26febcd956083
parkk <parkk@DESKTOP-0RE1N2Q>
2025-03-22 05:03:16 +0900

    250322_1

---
+++ src_250315/ex08class/QuTriangle.java
@@ -0,0 +1,34 @@
+package ex08class;
+
+class Triangle {
+	private int bottom;
+	private int height;
+	
+	public void init(int bottom, int height) {
+		this.bottom = bottom;
+		this.height = height;
+	}
+	public double getArea() {
+		return (bottom*height)/2.0;
+	}
+	public void setBottom(int bottom) {
+		this.bottom = bottom;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
+
+public class QuTriangle {
+	
+	public static void main(String[] args) {
+		Triangle t = new Triangle();
+		t.init(10, 17); //밑변10, 높이17로 초기화
+		System.out.println("삼각형의 넓이 : "+ t.getArea());
+		t.setBottom(50);//밑변 50으로 변경
+		t.setHeight(14);//높이 14로 변경
+		System.out.println("삼각형의 넓이 : "+ t.getArea());
+	}
+
+
+}
